ef62ac89d46350d54facc2b115280735df38723e
Soolj <101229764+Soolj@users.noreply.github.com>
2019-08-08 11:46:52 +0000

    use latest version

--- lib/matrix/Matrix.hpp
+++ lib/matrix/Matrix.hpp
@@ -42,4 +42,82 @@ public:
 
     Type _data[M][N];
 
-    vir
+    virtual ~Matrix() {};
+
+    Matrix() :
+        _data()
+    {
+    }
+
+    Matrix(const Type data_[][N]) :
+        _data()
+    {
+        memcpy(_data, data_, sizeof(_data));
+    }
+
+    Matrix(const Type *data_) :
+        _data()
+    {
+        memcpy(_data, data_, sizeof(_data));
+    }
+
+    Matrix(const Matrix &other) :
+        _data()
+    {
+        memcpy(_data, other._data, sizeof(_data));
+    }
+
+    /**
+     * Accessors/ Assignment etc.
+     */
+
+    Type *data()
+    {
+        return _data[0];
+    }
+
+    inline Type operator()(size_t i, size_t j) const
+    {
+        return _data[i][j];
+    }
+
+    inline Type &operator()(size_t i, size_t j)
+    {
+        return _data[i][j];
+    }
+
+    Matrix<Type, M, N> & operator=(const Matrix<Type, M, N> &other)
+    {
+        if (this != &other) {
+            memcpy(_data, other._data, sizeof(_data));
+        }
+        return (*this);
+    }
+
+    /**
+     * Matrix Operations
+     */
+
+    // this might use a lot of programming memory
+    // since it instantiates a class for every
+    // required mult pair, but it provides
+    // compile time size_t checking
+    template<size_t P>
+    Matrix<Type, M, P> operator*(const Matrix<Type, N, P> &other) const
+    {
+        const Matrix<Type, M, N> &self = *this;
+        Matrix<Type, M, P> res;
+        res.setZero();
+
+        for (size_t i = 0; i < M; i++) {
+            for (size_t k = 0; k < P; k++) {
+                for (size_t j = 0; j < N; j++) {
+                    res(i, k) += self(i, j) * other(j, k);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    Matrix<Type, M, N> emult(const Matrix<

954f2c4bc76016131b751ce9c0a139d8cf0d73f8
Soolj <101229764+Soolj@users.noreply.github.com>
2019-08-21 10:25:45 +0000

    rm CNAME

--- lib/matrix/Quaternion.hpp
+++ lib/matrix/Quaternion.hpp
@@ -65,4 +65,57 @@ public:
     Quaternion() :
         Vector<Type, 4>()
     {
-        Q
+        Quaternion &q = *this;
+        q(0) = 1;
+        q(1) = 0;
+        q(2) = 0;
+        q(3) = 0;
+    }
+
+    /**
+     * Constructor from Matrix41
+     *
+     * @param other Matrix41 to copy
+     */
+    Quaternion(const Matrix41 &other) :
+        Vector<Type, 4>(other)
+    {
+    }
+
+    /**
+     * Constructor from dcm
+     *
+     * Instance is initialized from a dcm representing coordinate transformation
+     * from frame 2 to frame 1.
+     *
+     * @param dcm dcm to set quaternion to
+     */
+    Quaternion(const Dcm<Type> &dcm) :
+        Vector<Type, 4>()
+    {
+        Quaternion &q = *this;
+        q(0) = Type(0.5) * Type(sqrt(Type(1) + dcm(0, 0) +
+                                     dcm(1, 1) + dcm(2, 2)));
+        q(1) = Type((dcm(2, 1) - dcm(1, 2)) /
+                    (Type(4) * q(0)));
+        q(2) = Type((dcm(0, 2) - dcm(2, 0)) /
+                    (Type(4) * q(0)));
+        q(3) = Type((dcm(1, 0) - dcm(0, 1)) /
+                    (Type(4) * q(0)));
+    }
+
+    /**
+     * Constructor from euler angles
+     *
+     * This sets the instance to a quaternion representing coordinate transformation from
+     * frame 2 to frame 1 where the rotation from frame 1 to frame 2 is described
+     * by a 3-2-1 intrinsic Tait-Bryan rotation sequence.
+     *
+     * @param euler euler angle instance
+     */
+    Quaternion(const Euler<Type> &euler) :
+        Vector<Type, 4>()
+    {
+        Quaternion &q = *this;
+        Type cosPhi_2 = Type(cos(euler.phi() / (Type)2.0));
+        Type cosTheta_2 = Type(

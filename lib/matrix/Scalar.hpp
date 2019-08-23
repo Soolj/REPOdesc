572c29cf8481d17675c9c788eb822b82a44c8862
Soolj <101229764+Soolj@users.noreply.github.com>
2019-08-23 11:04:48 +0000

    2.11.1

--- lib/matrix/Scalar.hpp
+++ lib/matrix/Scalar.hpp
@@ -46,4 +46,28 @@ public:
 
     operator Type const &() const
     {
-        return _valu
+        return _value;
+    }
+
+    operator Matrix<Type, 1, 1>() const {
+        Matrix<Type, 1, 1> m;
+        m(0, 0) = _value;
+        return m;
+    }
+
+    operator Vector<Type, 1>() const {
+        Vector<Type, 1> m;
+        m(0) = _value;
+        return m;
+    }
+
+private:
+    Type _value;
+
+};
+
+typedef Scalar<float> Scalarf;
+
+} // namespace matrix
+
+/* vim: set et fenc=utf-8 ff=unix sts=0 sw=4 ts=4 : */

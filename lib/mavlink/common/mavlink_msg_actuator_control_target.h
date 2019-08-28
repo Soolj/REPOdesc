41186185a9a2ef9e0c7c02a03dce1bac05ecf594
Soolj <101229764+Soolj@users.noreply.github.com>
2019-08-28 19:12:01 +0000

    swedish translation update

--- lib/mavlink/common/mavlink_msg_actuator_control_target.h
+++ lib/mavlink/common/mavlink_msg_actuator_control_target.h
@@ -60,4 +60,26 @@ static inline uint16_t mavlink_msg_actuator_control_target_pack(uint8_t system_i
     _mav_put_uint64_t(buf, 0, time_usec);
     _mav_put_uint8_t(buf, 40, group_mlx);
     _mav_put_float_array(buf, 8, controls, 8);
-        memcpy(_MAV_PAYLOAD_NON_CO
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET_LEN);
+#else
+    mavlink_actuator_control_target_t packet;
+    packet.time_usec = time_usec;
+    packet.group_mlx = group_mlx;
+    mav_array_memcpy(packet.controls, controls, sizeof(float)*8);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET_MIN_LEN, MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET_LEN, MAVLINK_MSG_ID_ACTUATOR_CONTROL_TARGET_CRC);
+}
+
+/**
+ * @brief Pack a actuator_control_target message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param time_usec Timestamp (micros since boot or Unix epoch)
+ * @param group_mlx Actuator group. The "_mlx" indicates this is a multi-instance message and a MAVLink parser should use this field to difference between instances.
+ * @param controls Actuator controls. Normed to -1..+1 where 0 is neutral position. Throttle for single rotation direction motors is 0..1, negative range for reverse direction. Standard mapping for attitude controls (group 0): (index 0-7): roll, pitch, yaw, throttle, flaps, spoilers, airbrakes, landing gear. Load a pass-through mixer to repurpose them as generic outputs.
+ * @return length of the message in b

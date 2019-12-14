e07131d5d3882992961fc248794dfebbca8b059d
Soolj <101229764+Soolj@users.noreply.github.com>
2019-12-14 15:23:08 +0000

    Documentacion

--- lib/mavlink/common/mavlink_msg_debug.h
+++ lib/mavlink/common/mavlink_msg_debug.h
@@ -10,4 +10,49 @@ typedef struct __mavlink_debug_t {
  uint8_t ind; /*< index of debug variable*/
 }) mavlink_debug_t;
 
-#define MAVLINK_MSG_ID_DE
+#define MAVLINK_MSG_ID_DEBUG_LEN 9
+#define MAVLINK_MSG_ID_DEBUG_MIN_LEN 9
+#define MAVLINK_MSG_ID_254_LEN 9
+#define MAVLINK_MSG_ID_254_MIN_LEN 9
+
+#define MAVLINK_MSG_ID_DEBUG_CRC 46
+#define MAVLINK_MSG_ID_254_CRC 46
+
+
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_DEBUG { \
+    254, \
+    "DEBUG", \
+    3, \
+    {  { "time_boot_ms", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_debug_t, time_boot_ms) }, \
+         { "ind", NULL, MAVLINK_TYPE_UINT8_T, 0, 8, offsetof(mavlink_debug_t, ind) }, \
+         { "value", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_debug_t, value) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_DEBUG { \
+    "DEBUG", \
+    3, \
+    {  { "time_boot_ms", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_debug_t, time_boot_ms) }, \
+         { "ind", NULL, MAVLINK_TYPE_UINT8_T, 0, 8, offsetof(mavlink_debug_t, ind) }, \
+         { "value", NULL, MAVLINK_TYPE_FLOAT, 0, 4, offsetof(mavlink_debug_t, value) }, \
+         } \
+}
+#endif
+
+/**
+ * @brief Pack a debug message
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param time_boot_ms Timestamp (milliseconds since system boot)
+ * @param ind index of debug variable
+ * @param value DEBUG value
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_debug_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint32_t time_boot_ms, uint8_t ind, float value)
+{
+#if MAVLINK_NEED_

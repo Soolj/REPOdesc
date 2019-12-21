7b6c24f6f735daa90e8ca4ef0c20ca1eb5ede7ac
Soolj <101229764+Soolj@users.noreply.github.com>
2019-12-21 16:20:30 +0000

    Ignore DS_Store

--- lib/mavlink/common/mavlink_msg_follow_target.h
+++ lib/mavlink/common/mavlink_msg_follow_target.h
@@ -33,4 +33,26 @@ typedef struct __mavlink_follow_target_t {
 #define MAVLINK_MSG_FOLLOW_TARGET_FIELD_POSITION_COV_LEN 3
 
 #if MAVLINK_COMMAND_24BIT
-#define
+#define MAVLINK_MESSAGE_INFO_FOLLOW_TARGET { \
+    144, \
+    "FOLLOW_TARGET", \
+    11, \
+    {  { "timestamp", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_follow_target_t, timestamp) }, \
+         { "est_capabilities", NULL, MAVLINK_TYPE_UINT8_T, 0, 92, offsetof(mavlink_follow_target_t, est_capabilities) }, \
+         { "lat", NULL, MAVLINK_TYPE_INT32_T, 0, 16, offsetof(mavlink_follow_target_t, lat) }, \
+         { "lon", NULL, MAVLINK_TYPE_INT32_T, 0, 20, offsetof(mavlink_follow_target_t, lon) }, \
+         { "alt", NULL, MAVLINK_TYPE_FLOAT, 0, 24, offsetof(mavlink_follow_target_t, alt) }, \
+         { "vel", NULL, MAVLINK_TYPE_FLOAT, 3, 28, offsetof(mavlink_follow_target_t, vel) }, \
+         { "acc", NULL, MAVLINK_TYPE_FLOAT, 3, 40, offsetof(mavlink_follow_target_t, acc) }, \
+         { "attitude_q", NULL, MAVLINK_TYPE_FLOAT, 4, 52, offsetof(mavlink_follow_target_t, attitude_q) }, \
+         { "rates", NULL, MAVLINK_TYPE_FLOAT, 3, 68, offsetof(mavlink_follow_target_t, rates) }, \
+         { "position_cov", NULL, MAVLINK_TYPE_FLOAT, 3, 80, offsetof(mavlink_follow_target_t, position_cov) }, \
+         { "custom_state", NULL, MAVLINK_TYPE_UINT64_T, 0, 8, offsetof(mavlink_follow_target_t, custom_state) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_FOLLOW_TARGET { \
+    "FOLLOW_TARGET", \
+    11, \
+    {  { "timestamp", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_follow_target_t, timestamp) }, \
+         { "est_capabilities", NULL, MAVLINK_TYPE_UINT8_T, 0, 92, o

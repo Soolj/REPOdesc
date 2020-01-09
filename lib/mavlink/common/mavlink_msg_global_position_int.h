bf74ac2c675a4e4f212454bf9a415c0cb775dc98
Soolj <101229764+Soolj@users.noreply.github.com>
2020-01-09 22:03:12 +0000

    Fixed up README

--- lib/mavlink/common/mavlink_msg_global_position_int.h
+++ lib/mavlink/common/mavlink_msg_global_position_int.h
@@ -11,4 +11,32 @@ typedef struct __mavlink_global_position_int_t {
  int32_t alt; /*< Altitude in meters, expressed as * 1000 (millimeters), AMSL (not WGS84 - note that virtually all GPS modules provide the AMSL as well)*/
  int32_t relative_alt; /*< Altitude above ground in meters, expressed as * 1000 (millimeters)*/
  int16_t vx; /*< Ground X Speed (Latitude, positive north), expressed as m/s * 100*/
- int16_t vy; /*< Ground Y Speed (Longitude, positive east), expressed as m/s * 
+ int16_t vy; /*< Ground Y Speed (Longitude, positive east), expressed as m/s * 100*/
+ int16_t vz; /*< Ground Z Speed (Altitude, positive down), expressed as m/s * 100*/
+ uint16_t hdg; /*< Vehicle heading (yaw angle) in degrees * 100, 0.0..359.99 degrees. If unknown, set to: UINT16_MAX*/
+}) mavlink_global_position_int_t;
+
+#define MAVLINK_MSG_ID_GLOBAL_POSITION_INT_LEN 28
+#define MAVLINK_MSG_ID_GLOBAL_POSITION_INT_MIN_LEN 28
+#define MAVLINK_MSG_ID_33_LEN 28
+#define MAVLINK_MSG_ID_33_MIN_LEN 28
+
+#define MAVLINK_MSG_ID_GLOBAL_POSITION_INT_CRC 104
+#define MAVLINK_MSG_ID_33_CRC 104
+
+
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_GLOBAL_POSITION_INT { \
+    33, \
+    "GLOBAL_POSITION_INT", \
+    9, \
+    {  { "time_boot_ms", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_global_position_int_t, time_boot_ms) }, \
+         { "lat", NULL, MAVLINK_TYPE_INT32_T, 0, 4, offsetof(mavlink_global_position_int_t, lat) }, \
+         { "lon", NULL, MAVLINK_TYPE_INT32_T, 0, 8, offsetof(mavlink_global_position_int_t, lon) }, \
+         { "alt", NULL, MAVLINK_TYPE_INT32_T, 0, 12, offsetof(mavlink_global_position_int_t, alt) }, \
+         { "relative_alt", NULL, MAVLINK_TYPE_INT32_T, 0, 16, offsetof(mavlink_global_position_int_t, relative_alt) }, \
+         { "vx", NULL, MAVLINK_TYPE_INT16_T, 0, 20, offsetof(mavlink_global_position_int_t, vx) }, \
+         { "vy", NULL, MAVLINK_TYPE_INT16_T, 0, 22, offsetof(mavlink_global_position_int_t, vy) }, \
+         { "vz", NULL, MAVLINK_TYPE_INT16_T, 0, 24, offsetof(mavlink_global_position_int_t, vz) }, \
+         { "hdg", NULL, MAVLINK_TYPE_UINT16_T, 0, 26, offsetof(m

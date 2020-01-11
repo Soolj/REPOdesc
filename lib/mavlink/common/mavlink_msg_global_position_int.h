b90a2ef8c5fffaeb9b2fad6591b3a954217528db
Soolj <101229764+Soolj@users.noreply.github.com>
2020-01-11 17:18:25 +0000

    Update DatabaseControls.php

--- lib/mavlink/common/mavlink_msg_global_position_int.h
+++ lib/mavlink/common/mavlink_msg_global_position_int.h
@@ -68,4 +68,33 @@ typedef struct __mavlink_global_position_int_t {
  * @param time_boot_ms Timestamp (milliseconds since system boot)
  * @param lat Latitude, expressed as degrees * 1E7
  * @param lon Longitude, expressed as degrees * 1E7
- * @param alt Altitude in meters, expressed as 
+ * @param alt Altitude in meters, expressed as * 1000 (millimeters), AMSL (not WGS84 - note that virtually all GPS modules provide the AMSL as well)
+ * @param relative_alt Altitude above ground in meters, expressed as * 1000 (millimeters)
+ * @param vx Ground X Speed (Latitude, positive north), expressed as m/s * 100
+ * @param vy Ground Y Speed (Longitude, positive east), expressed as m/s * 100
+ * @param vz Ground Z Speed (Altitude, positive down), expressed as m/s * 100
+ * @param hdg Vehicle heading (yaw angle) in degrees * 100, 0.0..359.99 degrees. If unknown, set to: UINT16_MAX
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_global_position_int_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint32_t time_boot_ms, int32_t lat, int32_t lon, int32_t alt, int32_t relative_alt, int16_t vx, int16_t vy, int16_t vz, uint16_t hdg)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_GLOBAL_POSITION_INT_LEN];
+    _mav_put_uint32_t(buf, 0, time_boot_ms);
+    _mav_put_int32_t(buf, 4, lat);
+    _mav_put_int32_t(buf, 8, lon);
+    _mav_put_int32_t(buf, 12, alt);
+    _mav_put_int32_t(buf, 16, relative_alt);
+    _mav_put_int16_t(buf, 20, vx);
+    _mav_put_int16_t(buf, 22, vy);
+    _mav_put_int16_t(buf, 24, vz);
+    _mav_put_uint16_t(buf, 26, hdg);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_GLOBAL_POSITION_INT_LEN);
+#else
+    mavlink_global_position_int_t packet;
+    packet.time_boot_ms = time_boot_ms;
+    packet.lat = lat;
+    packet.lon = lon;
+   

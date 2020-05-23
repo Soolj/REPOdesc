4ac005b02e2e0c85ceddc44bbf81d2f3de34d667
Soolj <101229764+Soolj@users.noreply.github.com>
2020-05-23 16:27:16 +0000

    Update Travis CI configuration

--- lib/mavlink/common/mavlink_msg_gps2_rtk.h
+++ lib/mavlink/common/mavlink_msg_gps2_rtk.h
@@ -9,4 +9,35 @@ typedef struct __mavlink_gps2_rtk_t {
  uint32_t tow; /*< GPS Time of Week of last baseline*/
  int32_t baseline_a_mm; /*< Current baseline in ECEF x or NED north component in mm.*/
  int32_t baseline_b_mm; /*< Current baseline in ECEF y or NED east component in mm.*/
- int32_t baseline_c_mm; /*< Current baseline
+ int32_t baseline_c_mm; /*< Current baseline in ECEF z or NED down component in mm.*/
+ uint32_t accuracy; /*< Current estimate of baseline accuracy.*/
+ int32_t iar_num_hypotheses; /*< Current number of integer ambiguity hypotheses.*/
+ uint16_t wn; /*< GPS Week Number of last baseline*/
+ uint8_t rtk_receiver_id; /*< Identification of connected RTK receiver.*/
+ uint8_t rtk_health; /*< GPS-specific health report for RTK data.*/
+ uint8_t rtk_rate; /*< Rate of baseline messages being received by GPS, in HZ*/
+ uint8_t nsats; /*< Current number of sats used for RTK calculation.*/
+ uint8_t baseline_coords_type; /*< Coordinate system of baseline*/
+}) mavlink_gps2_rtk_t;
+
+#define MAVLINK_MSG_ID_GPS2_RTK_LEN 35
+#define MAVLINK_MSG_ID_GPS2_RTK_MIN_LEN 35
+#define MAVLINK_MSG_ID_128_LEN 35
+#define MAVLINK_MSG_ID_128_MIN_LEN 35
+
+#define MAVLINK_MSG_ID_GPS2_RTK_CRC 226
+#define MAVLINK_MSG_ID_128_CRC 226
+
+
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_GPS2_RTK { \
+    128, \
+    "GPS2_RTK", \
+    13, \
+    {  { "time_last_baseline_ms", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_gps2_rtk_t, time_last_baseline_ms) }, \
+         { "rtk_receiver_id", NULL, MAVLINK_TYPE_UINT8_T, 0, 30, offsetof(mavlink_gps2_rtk_t, rtk_receiver_id) }, \
+         { "wn", NULL, MAVLINK_TYPE_UINT16_T, 0, 28, offsetof(mavlink_gps2_rtk_t, wn) }, \
+         { "tow", NULL, MAVLINK_TYPE_UINT32_T, 0, 4, offsetof(mavlink_gps2_rtk_t, tow) }, \
+         { "rtk_health", NULL, MAVLINK_TYPE_UINT8_T, 0, 31, offsetof(mavlink_gps2_rtk_t, rtk_health) }, \
+         { "rtk_rate", NULL, MAVLINK_TYP

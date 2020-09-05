15d7faa7fb9b3d86865113a78c4f68f3b5ee363d
Soolj <101229764+Soolj@users.noreply.github.com>
2020-09-05 22:38:57 +0000

    staging: rtl8712: Add device ID for Sitecom WLA2100

--- lib/mavlink/common/mavlink_msg_heartbeat.h
+++ lib/mavlink/common/mavlink_msg_heartbeat.h
@@ -63,4 +63,40 @@ typedef struct __mavlink_heartbeat_t {
  * @param system_status System status flag, see MAV_STATE ENUM
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_
+static inline uint16_t mavlink_msg_heartbeat_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint8_t type, uint8_t autopilot, uint8_t base_mode, uint32_t custom_mode, uint8_t system_status)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_HEARTBEAT_LEN];
+    _mav_put_uint32_t(buf, 0, custom_mode);
+    _mav_put_uint8_t(buf, 4, type);
+    _mav_put_uint8_t(buf, 5, autopilot);
+    _mav_put_uint8_t(buf, 6, base_mode);
+    _mav_put_uint8_t(buf, 7, system_status);
+    _mav_put_uint8_t(buf, 8, 3);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_HEARTBEAT_LEN);
+#else
+    mavlink_heartbeat_t packet;
+    packet.custom_mode = custom_mode;
+    packet.type = type;
+    packet.autopilot = autopilot;
+    packet.base_mode = base_mode;
+    packet.system_status = system_status;
+    packet.mavlink_version = 3;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_HEARTBEAT_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_HEARTBEAT;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_HEARTBEAT_MIN_LEN, MAVLINK_MSG_ID_HEARTBEAT_LEN, MAVLINK_MSG_ID_HEARTBEAT_CRC);
+}
+
+/**
+ * @brief Pack a heartbeat message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param type Type of the MAV (quadrotor, helicopter, etc., up to 15 types, defined in MAV_TYPE ENUM)
+ * @param autopilot Autopilot type / class. defined in MAV_AUTOP

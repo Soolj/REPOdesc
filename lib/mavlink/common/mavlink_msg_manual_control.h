dd6379e10a851170007188edee272b84447f9338
Soolj <101229764+Soolj@users.noreply.github.com>
2021-01-16 07:38:33 +0000

    xtensa: fix threadptr reload on return to userspace

--- lib/mavlink/common/mavlink_msg_manual_control.h
+++ lib/mavlink/common/mavlink_msg_manual_control.h
@@ -54,4 +54,17 @@ typedef struct __mavlink_manual_control_t {
  * @brief Pack a manual_control message
  * @param system_id ID of this system
  * @param component_id ID of this component (e.g. 200 for IMU)
- * @param msg The MAVLink 
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param target The system to be controlled.
+ * @param x X-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to forward(1000)-backward(-1000) movement on a joystick and the pitch of a vehicle.
+ * @param y Y-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to left(-1000)-right(1000) movement on a joystick and the roll of a vehicle.
+ * @param z Z-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to a separate slider movement with maximum being 1000 and minimum being -1000 on a joystick and the thrust of a vehicle. Positive values are positive thrust, negative values are negative thrust.
+ * @param r R-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to a twisting of the joystick, with counter-clockwise being 1000 and clockwise being -1000, and the yaw of a vehicle.
+ * @param buttons A bitfield corresponding to the joystick buttons' current state, 1 for pressed, 0 for released. The lowest bit corresponds to Button 1.
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_manual_control_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint8_t target, int16_t x, int16_t y, int16_t z, int16_t r, uint16_t buttons)
+{
+#if MAVLINK_NEED

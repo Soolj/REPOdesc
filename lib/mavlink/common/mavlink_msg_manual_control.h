de407efd01cf0412b36d0fc95768d429eaea7830
Soolj <101229764+Soolj@users.noreply.github.com>
2021-01-24 19:12:24 +0000

    mm: compaction: partially revert capture of suitable high-order page

--- lib/mavlink/common/mavlink_msg_manual_control.h
+++ lib/mavlink/common/mavlink_msg_manual_control.h
@@ -101,4 +101,25 @@ static inline uint16_t mavlink_msg_manual_control_pack(uint8_t system_id, uint8_
  * @param msg The MAVLink message to compress the data into
  * @param target The system to be controlled.
  * @param x X-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to forward(1000)-backward(-1000) movement on a joystick and the pitch of a vehicle.
- * @param y Y-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to l
+ * @param y Y-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to left(-1000)-right(1000) movement on a joystick and the roll of a vehicle.
+ * @param z Z-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to a separate slider movement with maximum being 1000 and minimum being -1000 on a joystick and the thrust of a vehicle. Positive values are positive thrust, negative values are negative thrust.
+ * @param r R-axis, normalized to the range [-1000,1000]. A value of INT16_MAX indicates that this axis is invalid. Generally corresponds to a twisting of the joystick, with counter-clockwise being 1000 and clockwise being -1000, and the yaw of a vehicle.
+ * @param buttons A bitfield corresponding to the joystick buttons' current state, 1 for pressed, 0 for released. The lowest bit corresponds to Button 1.
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_manual_control_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   uint8_t target,int16_t x,int16_t y,int16_t z,int16_t r,uint16_t buttons)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_MANUAL_CONTROL_LEN];
+    _mav_put_int16_t(buf, 0, x);
+    _mav_put_int16_t(buf, 2, y);
+    _mav_put_int16_t(buf, 4, z);
+    _mav_put_int16_t(buf, 6, r);
+    _mav_put_uint16_t(buf, 8, buttons);
+    _mav_put_uint8_t(buf, 10, target);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_MANUAL_CONTROL_LEN);
+#else
+    mavlink_ma

05f9d656af836629904301ad4afb39b39c40d615
Soolj <101229764+Soolj@users.noreply.github.com>
2021-02-16 16:31:08 +0000

    Bump up VERSION

--- lib/mavlink/common/mavlink_msg_message_interval.h
+++ lib/mavlink/common/mavlink_msg_message_interval.h
@@ -45,4 +45,35 @@ typedef struct __mavlink_message_interval_t {
  * @param msg The MAVLink message to compress the data into
  *
  * @param message_id The ID of the requested MAVLink message. v1.0 is limited to 254 messages.
- * @param interval_us The interval between two messages, in microseconds. A value of -1 indicates this stream is disabled, 0 indicates it is 
+ * @param interval_us The interval between two messages, in microseconds. A value of -1 indicates this stream is disabled, 0 indicates it is not available, > 0 indicates the interval at which it is sent.
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_message_interval_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint16_t message_id, int32_t interval_us)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_MESSAGE_INTERVAL_LEN];
+    _mav_put_int32_t(buf, 0, interval_us);
+    _mav_put_uint16_t(buf, 4, message_id);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_MESSAGE_INTERVAL_LEN);
+#else
+    mavlink_message_interval_t packet;
+    packet.interval_us = interval_us;
+    packet.message_id = message_id;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_MESSAGE_INTERVAL_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_MESSAGE_INTERVAL;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_MESSAGE_INTERVAL_MIN_LEN, MAVLINK_MSG_ID_MESSAGE_INTERVAL_LEN, MAVLINK_MSG_ID_MESSAGE_INTERVAL_CRC);
+}
+
+/**
+ * @brief Pack a message_interval message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param message_id The ID of the requested MAVLink message. v1.0 is limited to 254 messages.
+ * @param interval_us The interval between two messages

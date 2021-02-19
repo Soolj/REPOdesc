6972f09aa3dfea91d18d2db2641efafbc7483ea5
Soolj <101229764+Soolj@users.noreply.github.com>
2021-02-19 15:35:28 +0000

    Update domain.txt

--- lib/mavlink/common/mavlink_msg_message_interval.h
+++ lib/mavlink/common/mavlink_msg_message_interval.h
@@ -109,4 +109,35 @@ static inline uint16_t mavlink_msg_message_interval_pack_chan(uint8_t system_id,
  * @param msg The MAVLink message to compress the data into
  * @param message_interval C-struct to read the message contents from
  */
-static inline uint16_t mavlink_msg_message_interval_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t*
+static inline uint16_t mavlink_msg_message_interval_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_message_interval_t* message_interval)
+{
+    return mavlink_msg_message_interval_pack(system_id, component_id, msg, message_interval->message_id, message_interval->interval_us);
+}
+
+/**
+ * @brief Encode a message_interval struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param message_interval C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_message_interval_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_message_interval_t* message_interval)
+{
+    return mavlink_msg_message_interval_pack_chan(system_id, component_id, chan, msg, message_interval->message_id, message_interval->interval_us);
+}
+
+/**
+ * @brief Send a message_interval message
+ * @param chan MAVLink channel to send the message
+ *
+ * @param message_id The ID of the requested MAVLink message. v1.0 is limited to 254 messages.
+ * @param interval_us The interval between two messages, in microseconds. A value of -1 indicates this stream is disabled, 0 indicates it is not available, > 0 indicates the interval at which it is sent.
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_message_interval_send(mavlink_channel_t chan, uint16_t message_id, int32_t interval_us)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_

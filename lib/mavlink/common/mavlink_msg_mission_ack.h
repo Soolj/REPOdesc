ed369f594055963192639f9abd65897649e7c133
Soolj <101229764+Soolj@users.noreply.github.com>
2021-03-01 21:57:35 +0000

    Update articles.css

--- lib/mavlink/common/mavlink_msg_mission_ack.h
+++ lib/mavlink/common/mavlink_msg_mission_ack.h
@@ -14,3 +14,42 @@ typedef struct __mavlink_mission_ack_t {
 #define MAVLINK_MSG_ID_MISSION_ACK_LEN 4
 #define MAVLINK_MSG_ID_MISSION_ACK_MIN_LEN 3
 #define MAVLINK_MSG_ID_47_LEN 4
+#define MAVLINK_MSG_ID_47_MIN_LEN 3
+
+#define MAVLINK_MSG_ID_MISSION_ACK_CRC 153
+#define MAVLINK_MSG_ID_47_CRC 153
+
+
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_MISSION_ACK { \
+    47, \
+    "MISSION_ACK", \
+    4, \
+    {  { "target_system", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_mission_ack_t, target_system) }, \
+         { "target_component", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_mission_ack_t, target_component) }, \
+         { "type", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_mission_ack_t, type) }, \
+         { "mission_type", NULL, MAVLINK_TYPE_UINT8_T, 0, 3, offsetof(mavlink_mission_ack_t, mission_type) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_MISSION_ACK { \
+    "MISSION_ACK", \
+    4, \
+    {  { "target_system", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_mission_ack_t, target_system) }, \
+         { "target_component", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_mission_ack_t, target_component) }, \
+         { "type", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_mission_ack_t, type) }, \
+         { "mission_type", NULL, MAVLINK_TYPE_UINT8_T, 0, 3, offsetof(mavlink_mission_ack_t, mission_type) }, \
+         } \
+}
+#endif
+
+/**
+ * @brief Pack a mission_ack message
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param target_system System ID
+ * @param target_component Component ID
+ * @param type See MAV_MISSION_RESULT enum

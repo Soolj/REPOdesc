ff5fcdd5e2fc79823f7012a48447980dbef4aa3e
Soolj <101229764+Soolj@users.noreply.github.com>
2021-03-23 21:14:41 +0000

    Delete arabic_lock.lua

--- lib/mavlink/common/mavlink_msg_mission_set_current.h
+++ lib/mavlink/common/mavlink_msg_mission_set_current.h
@@ -56,4 +56,40 @@ static inline uint16_t mavlink_msg_mission_set_current_pack(uint8_t system_id, u
                                uint8_t target_system, uint8_t target_component, uint16_t seq)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    char buf[MAVLINK_MS
+    char buf[MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN];
+    _mav_put_uint16_t(buf, 0, seq);
+    _mav_put_uint8_t(buf, 2, target_system);
+    _mav_put_uint8_t(buf, 3, target_component);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN);
+#else
+    mavlink_mission_set_current_t packet;
+    packet.seq = seq;
+    packet.target_system = target_system;
+    packet.target_component = target_component;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_MISSION_SET_CURRENT;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_MISSION_SET_CURRENT_MIN_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_CRC);
+}
+
+/**
+ * @brief Pack a mission_set_current message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param target_system System ID
+ * @param target_component Component ID
+ * @param seq Sequence
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_mission_set_current_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   uint8_t target_system,uint8_t target_component,uint16_t seq)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN];
+    

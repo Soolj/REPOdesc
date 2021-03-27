53e08be111efce988a3c9e9865c5360e4afd9187
Soolj <101229764+Soolj@users.noreply.github.com>
2021-03-27 08:11:22 +0000

    removed useless file

--- lib/mavlink/common/mavlink_msg_mission_set_current.h
+++ lib/mavlink/common/mavlink_msg_mission_set_current.h
@@ -188,4 +188,54 @@ static inline void mavlink_msg_mission_set_current_send_struct(mavlink_channel_t
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_mission_set_current_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t target_system, uint8_t target_compo
+static inline void mavlink_msg_mission_set_current_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t target_system, uint8_t target_component, uint16_t seq)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char *buf = (char *)msgbuf;
+    _mav_put_uint16_t(buf, 0, seq);
+    _mav_put_uint8_t(buf, 2, target_system);
+    _mav_put_uint8_t(buf, 3, target_component);
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_MISSION_SET_CURRENT, buf, MAVLINK_MSG_ID_MISSION_SET_CURRENT_MIN_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_CRC);
+#else
+    mavlink_mission_set_current_t *packet = (mavlink_mission_set_current_t *)msgbuf;
+    packet->seq = seq;
+    packet->target_system = target_system;
+    packet->target_component = target_component;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_MISSION_SET_CURRENT, (const char *)packet, MAVLINK_MSG_ID_MISSION_SET_CURRENT_MIN_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_LEN, MAVLINK_MSG_ID_MISSION_SET_CURRENT_CRC);
+#endif
+}
+#endif
+
+#endif
+
+// MESSAGE MISSION_SET_CURRENT UNPACKING
+
+
+/**
+ * @brief Get field target_system from mission_set_current message
+ *
+ * @return System ID
+ */
+static inline uint8_t mavlink_msg_mission_set_current_get_target_system(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_uint8_t(msg,  2);
+}
+
+/**
+ * @brief Get field target_component from mission_set_current message
+ *
+ * @return Component ID
+ */
+static inline uint8_t mavlink_msg_mission_set_current_get_target_component(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_uint8_t(msg,  3);
+}
+
+/**
+ * @brief Get field seq from mission_set_current message
+ *
+ * @return Sequence
+ */
+static inline uint16

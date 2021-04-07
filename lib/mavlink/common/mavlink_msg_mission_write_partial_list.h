51b2b82757ea7a246b68d39a1ff68fb1facece20
Soolj <101229764+Soolj@users.noreply.github.com>
2021-04-07 06:10:12 +0000

    version 1.0.2

--- lib/mavlink/common/mavlink_msg_mission_write_partial_list.h
+++ lib/mavlink/common/mavlink_msg_mission_write_partial_list.h
@@ -214,4 +214,48 @@ static inline void mavlink_msg_mission_write_partial_list_send_struct(mavlink_ch
  */
 static inline void mavlink_msg_mission_write_partial_list_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t target_system, uint8_t target_component, int16_t start_index, int16_t end_index, uint8_t mission_type)
 {
-#if MAVLINK_NEED_BY
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char *buf = (char *)msgbuf;
+    _mav_put_int16_t(buf, 0, start_index);
+    _mav_put_int16_t(buf, 2, end_index);
+    _mav_put_uint8_t(buf, 4, target_system);
+    _mav_put_uint8_t(buf, 5, target_component);
+    _mav_put_uint8_t(buf, 6, mission_type);
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST, buf, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_MIN_LEN, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_LEN, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_CRC);
+#else
+    mavlink_mission_write_partial_list_t *packet = (mavlink_mission_write_partial_list_t *)msgbuf;
+    packet->start_index = start_index;
+    packet->end_index = end_index;
+    packet->target_system = target_system;
+    packet->target_component = target_component;
+    packet->mission_type = mission_type;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST, (const char *)packet, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_MIN_LEN, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_LEN, MAVLINK_MSG_ID_MISSION_WRITE_PARTIAL_LIST_CRC);
+#endif
+}
+#endif
+
+#endif
+
+// MESSAGE MISSION_WRITE_PARTIAL_LIST UNPACKING
+
+
+/**
+ * @brief Get field target_system from mission_write_partial_list message
+ *
+ * @return System ID
+ */
+static inline uint8_t mavlink_msg_mission_write_partial_list_get_target_system(const mavlink_message_t* msg)
+{
+    return _MAV_RETURN_uint8_t(msg,  4);
+}
+
+/**
+ * @brief Get field target_component from mission_write_partial_list message
+ *
+ * @return Component ID
+ */
+static inline uint8_t mavlink_msg_mission_write_partial_list_get_target_component(const mavlink_message_t* msg)
+{
+    retu

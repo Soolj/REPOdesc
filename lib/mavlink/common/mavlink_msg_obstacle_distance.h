41f80291c8ab7762079a06225b2e2896e560a851
Soolj <101229764+Soolj@users.noreply.github.com>
2021-04-12 13:06:27 +0000

    Coq 8.9.0, repo released, mode clean.

--- lib/mavlink/common/mavlink_msg_obstacle_distance.h
+++ lib/mavlink/common/mavlink_msg_obstacle_distance.h
@@ -52,4 +52,26 @@ typedef struct __mavlink_obstacle_distance_t {
 
 /**
  * @brief Pack a obstacle_distance message
- * @pa
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ *
+ * @param time_usec Timestamp (microseconds since system boot or since UNIX epoch)
+ * @param sensor_type Class id of the distance sensor type.
+ * @param distances Distance of obstacles in front of the sensor starting on the left side. A value of 0 means that the obstacle is right in front of the sensor. A value of max_distance +1 means no obstace is present. A value of UINT16_MAX for unknown/not used. In a array element, each unit corresponds to 1cm.
+ * @param increment Angular width in degrees of each array element.
+ * @param min_distance Minimum distance the sensor can measure in centimeters
+ * @param max_distance Maximum distance the sensor can measure in centimeters
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_obstacle_distance_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint64_t time_usec, uint8_t sensor_type, const uint16_t *distances, uint8_t increment, uint16_t min_distance, uint16_t max_distance)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_OBSTACLE_DISTANCE_LEN];
+    _mav_put_uint64_t(buf, 0, time_usec);
+    _mav_put_uint16_t(buf, 152, min_distance);
+    _mav_put_uint16_t(buf, 154, max_distance);
+    _mav_put_uint8_t(buf, 156, sensor_type);
+    _mav_put_uint8_t(buf, 157, increment);
+    _mav_put_uint16_t_arr

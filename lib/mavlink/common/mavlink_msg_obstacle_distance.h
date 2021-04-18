c8d51f6ba1c9b13ab33a0bacf4c5acd685fd536c
Soolj <101229764+Soolj@users.noreply.github.com>
2021-04-18 19:31:15 +0000

    fiddling

--- lib/mavlink/common/mavlink_msg_obstacle_distance.h
+++ lib/mavlink/common/mavlink_msg_obstacle_distance.h
@@ -206,4 +206,33 @@ static inline void mavlink_msg_obstacle_distance_send_struct(mavlink_channel_t c
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     mavlink_msg_obstacle_distance_send(chan, obstacle_distance->time_usec, obstacle_distance->sensor_type, obstacle_distance->distances, obstacle_distance->increment, obstacle_distance->min_distance, obstacle_distance->max_distance);
 #else
-    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_OBSTACLE_DISTANCE, (const char *)o
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_OBSTACLE_DISTANCE, (const char *)obstacle_distance, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_MIN_LEN, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_LEN, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_CRC);
+#endif
+}
+
+#if MAVLINK_MSG_ID_OBSTACLE_DISTANCE_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is the size of a full mavlink message. This
+  is usually the receive buffer for the channel, and allows a reply to an
+  incoming message with minimum stack space usage.
+ */
+static inline void mavlink_msg_obstacle_distance_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint64_t time_usec, uint8_t sensor_type, const uint16_t *distances, uint8_t increment, uint16_t min_distance, uint16_t max_distance)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char *buf = (char *)msgbuf;
+    _mav_put_uint64_t(buf, 0, time_usec);
+    _mav_put_uint16_t(buf, 152, min_distance);
+    _mav_put_uint16_t(buf, 154, max_distance);
+    _mav_put_uint8_t(buf, 156, sensor_type);
+    _mav_put_uint8_t(buf, 157, increment);
+    _mav_put_uint16_t_array(buf, 8, distances, 72);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_OBSTACLE_DISTANCE, buf, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_MIN_LEN, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_LEN, MAVLINK_MSG_ID_OBSTACLE_DISTANCE_CRC);
+#else
+    mavlink_obstacle_distance_t *packet = (mavlink_obstacle_distance_t *)msgbuf;
+    packet->time_usec = time_usec;
+    packet->min_distance = min_distance;
+    packet->max_distance = max_distance;
+    packet->sensor_type = sensor_type;
+    packet->incre

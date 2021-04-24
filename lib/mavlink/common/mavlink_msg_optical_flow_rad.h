c2cea263422385301a6fbf4e040e796be73dc629
Soolj <101229764+Soolj@users.noreply.github.com>
2021-04-24 18:27:44 +0000

    - uprev version

--- lib/mavlink/common/mavlink_msg_optical_flow_rad.h
+++ lib/mavlink/common/mavlink_msg_optical_flow_rad.h
@@ -77,4 +77,22 @@ typedef struct __mavlink_optical_flow_rad_t {
  * @param time_usec Timestamp (microseconds, synced to UNIX time or since system boot)
  * @param sensor_id Sensor ID
  * @param integration_time_us Integration time in microseconds. Divide integrated_x and integrated_y by the integration time to obtain average flow. The integration time also indicates the.
- * @param integrated_x Flow in radians around X axis (Sensor RH rotation about the X axis induces a positive flow. Sensor linear motion al
+ * @param integrated_x Flow in radians around X axis (Sensor RH rotation about the X axis induces a positive flow. Sensor linear motion along the positive Y axis induces a negative flow.)
+ * @param integrated_y Flow in radians around Y axis (Sensor RH rotation about the Y axis induces a positive flow. Sensor linear motion along the positive X axis induces a positive flow.)
+ * @param integrated_xgyro RH rotation around X axis (rad)
+ * @param integrated_ygyro RH rotation around Y axis (rad)
+ * @param integrated_zgyro RH rotation around Z axis (rad)
+ * @param temperature Temperature * 100 in centi-degrees Celsius
+ * @param quality Optical flow quality / confidence. 0: no valid flow, 255: maximum quality
+ * @param time_delta_distance_us Time in microseconds since the distance was sampled.
+ * @param distance Distance to the center of the flow field in meters. Positive value (including zero): distance known. Negative value: Unknown distance.
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_optical_flow_rad_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint64_t time_usec, uint8_t sensor_id, uint32_t integration_time_us, float integrated_x, float integrated_y, float integrated_xgyro, float integrated_ygyro, float integrated_zgyro, int16_t temperature, uint8_t quality, uint32_t time_delta_distance_us, float distance)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_OPTICAL_FLOW_RAD_LEN];
+    _mav_put_uint64_t(buf, 0, time_usec);
+    _mav_put_uint32_t(buf, 8, integration_time_us);
+    _mav_put_float(buf, 12, integrated_x)

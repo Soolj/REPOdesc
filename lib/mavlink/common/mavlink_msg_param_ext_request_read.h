25b8f7069bed9498035ce9ef3dd42fc9171431be
Soolj <101229764+Soolj@users.noreply.github.com>
2021-05-09 16:57:44 +0000

    v0.9.10

--- lib/mavlink/common/mavlink_msg_param_ext_request_read.h
+++ lib/mavlink/common/mavlink_msg_param_ext_request_read.h
@@ -14,4 +14,37 @@ typedef struct __mavlink_param_ext_request_read_t {
 #define MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_LEN 20
 #define MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_MIN_LEN 20
 #define MAVLINK_MSG_ID_320_LEN 20
-#define MAVL
+#define MAVLINK_MSG_ID_320_MIN_LEN 20
+
+#define MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_CRC 243
+#define MAVLINK_MSG_ID_320_CRC 243
+
+#define MAVLINK_MSG_PARAM_EXT_REQUEST_READ_FIELD_PARAM_ID_LEN 16
+
+#if MAVLINK_COMMAND_24BIT
+#define MAVLINK_MESSAGE_INFO_PARAM_EXT_REQUEST_READ { \
+    320, \
+    "PARAM_EXT_REQUEST_READ", \
+    4, \
+    {  { "target_system", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_param_ext_request_read_t, target_system) }, \
+         { "target_component", NULL, MAVLINK_TYPE_UINT8_T, 0, 3, offsetof(mavlink_param_ext_request_read_t, target_component) }, \
+         { "param_id", NULL, MAVLINK_TYPE_CHAR, 16, 4, offsetof(mavlink_param_ext_request_read_t, param_id) }, \
+         { "param_index", NULL, MAVLINK_TYPE_INT16_T, 0, 0, offsetof(mavlink_param_ext_request_read_t, param_index) }, \
+         } \
+}
+#else
+#define MAVLINK_MESSAGE_INFO_PARAM_EXT_REQUEST_READ { \
+    "PARAM_EXT_REQUEST_READ", \
+    4, \
+    {  { "target_system", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_param_ext_request_read_t, target_system) }, \
+         { "target_component", NULL, MAVLINK_TYPE_UINT8_T, 0, 3, offsetof(mavlink_param_ext_request_read_t, target_component) }, \
+         { "param_id", NULL, MAVLINK_TYPE_CHAR, 16, 4, offsetof(mavlink_param_ext_request_read_t, param_id) }, \
+         { "param_index", NULL, MAVLINK_TYPE_INT16_T, 0, 0, offsetof(mavlink_param_ext_request_read_t, param_index) }, \
+         } \
+}
+#endif
+
+/**
+ * @brief Pack a param_ext_request_read message
+ * @param system_id ID of this system
+ * @param component_id ID of this compon

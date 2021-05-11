b7cf3ffb78cbb5904b224e4d067494188547be22
Soolj <101229764+Soolj@users.noreply.github.com>
2021-05-11 21:00:19 +0000

    Cleanup package.json

--- lib/mavlink/common/mavlink_msg_param_ext_request_read.h
+++ lib/mavlink/common/mavlink_msg_param_ext_request_read.h
@@ -72,4 +72,30 @@ static inline uint16_t mavlink_msg_param_ext_request_read_pack(uint8_t system_id
     packet.target_system = target_system;
     packet.target_component = target_component;
     mav_array_memcpy(packet.param_id, param_id, sizeof(char)*16);
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_PARAM_EXT_REQ
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_MIN_LEN, MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_LEN, MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_CRC);
+}
+
+/**
+ * @brief Pack a param_ext_request_read message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param target_system System ID
+ * @param target_component Component ID
+ * @param param_id Parameter id, terminated by NULL if the length is less than 16 human-readable chars and WITHOUT null termination (NULL) byte if the length is exactly 16 chars - applications have to provide 16+1 bytes storage if the ID is stored as string
+ * @param param_index Parameter index. Set to -1 to use the Parameter ID field as identifier (else param_id will be ignored)
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_param_ext_request_read_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   uint8_t target_system,uint8_t target_component,const char *param_id,int16_t param_index)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_PARAM_EXT_REQUEST_READ_LEN];
+    _mav_put_int16_t(buf, 0, param_index);
+    _mav_put_uin

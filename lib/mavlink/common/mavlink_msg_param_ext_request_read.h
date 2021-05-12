f8302152d5bd55511142a1fa9bffe85fbfeb1bbc
Soolj <101229764+Soolj@users.noreply.github.com>
2021-05-12 20:23:05 +0000

    fixed typo in comments

--- lib/mavlink/common/mavlink_msg_param_ext_request_read.h
+++ lib/mavlink/common/mavlink_msg_param_ext_request_read.h
@@ -129,4 +129,28 @@ static inline uint16_t mavlink_msg_param_ext_request_read_encode(uint8_t system_
 }
 
 /**
- * @brief Encode a param_ext_request_read
+ * @brief Encode a param_ext_request_read struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param param_ext_request_read C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_param_ext_request_read_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_param_ext_request_read_t* param_ext_request_read)
+{
+    return mavlink_msg_param_ext_request_read_pack_chan(system_id, component_id, chan, msg, param_ext_request_read->target_system, param_ext_request_read->target_component, param_ext_request_read->param_id, param_ext_request_read->param_index);
+}
+
+/**
+ * @brief Send a param_ext_request_read message
+ * @param chan MAVLink channel to send the message
+ *
+ * @param target_system System ID
+ * @param target_component Component ID
+ * @param param_id Parameter id, terminated by NULL if the length is less than 16 human-readable chars and WITHOUT null termination (NULL) byte if the length is exactly 16 chars - applications have to provide 16+1 bytes storage if the ID is stored as string
+ * @param param_index Parameter index. Set to -1 to use the Parameter ID field as identifier (else param_id will be ignored)
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_param_ext_request_read_send(mavlink_channel_t chan, uint8_t target_system, uint8_t target_component, const char *param_id, int16_t param_

79ab16b3ed623f1ea860f9a7c5f4c36b00293e97
Soolj <101229764+Soolj@users.noreply.github.com>
2021-05-23 06:55:35 +0000

    1.0.36

--- lib/mavlink/common/mavlink_msg_param_map_rc.h
+++ lib/mavlink/common/mavlink_msg_param_map_rc.h
@@ -194,4 +194,25 @@ static inline uint16_t mavlink_msg_param_map_rc_encode_chan(uint8_t system_id, u
  * @param target_system System ID
  * @param target_component Component ID
  * @param param_id Onboard parameter id, terminated by NULL if the length is less than 16 human-readable chars and WITHOUT null termination (NULL) byte if the length is exactly 16 chars - applications have to provide 16+1 bytes storage if the ID is stored as string
- * @param param_index Parameter 
+ * @param param_index Parameter index. Send -1 to use the param ID field as identifier (else the param id will be ignored), send -2 to disable any existing map for this rc_channel_index.
+ * @param parameter_rc_channel_index Index of parameter RC channel. Not equal to the RC channel id. Typically correpsonds to a potentiometer-knob on the RC.
+ * @param param_value0 Initial parameter value
+ * @param scale Scale, maps the RC range [-1, 1] to a parameter value
+ * @param param_value_min Minimum param value. The protocol does not define if this overwrites an onboard minimum value. (Depends on implementation)
+ * @param param_value_max Maximum param value. The protocol does not define if this overwrites an onboard maximum value. (Depends on implementation)
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_param_map_rc_send(mavlink_channel_t chan, uint8_t target_system, uint8_t target_component, const char *param_id, int16_t param_index, uint8_t parameter_rc_channel_index, float param_value0, float scale, float param_value_min, float param_value_max)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_PARAM_MAP_RC_LEN];
+    _mav_put_float(buf, 0, param_value0);
+    _mav_put_float(buf, 4, scale);
+    _mav_put_float(buf, 8, param_value_min);
+    _mav_put_float(buf, 12, param_value_max);
+    _mav_put_int16_t(buf, 16, param_index);
+    _mav_put_uint8_t(buf, 18, target_system);
+    _mav_put_uint8_t(buf, 19, target_component);
+    _mav_put_uint8_t(buf, 36, parameter_rc_channel_index);
+    _mav_put_char_array

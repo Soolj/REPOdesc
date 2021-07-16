91187f09b3c2c03f79027e6d30ff3b3542f0c8bc
Soolj <101229764+Soolj@users.noreply.github.com>
2021-07-16 19:50:55 +0000

    mm: fix vma_resv_map() NULL pointer

--- lib/mavlink/common/mavlink_msg_power_status.h
+++ lib/mavlink/common/mavlink_msg_power_status.h
@@ -52,4 +52,38 @@ typedef struct __mavlink_power_status_t {
  * @param flags power supply status flags (see MAV_POWER_STATUS enum)
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inl
+static inline uint16_t mavlink_msg_power_status_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
+                               uint16_t Vcc, uint16_t Vservo, uint16_t flags)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_POWER_STATUS_LEN];
+    _mav_put_uint16_t(buf, 0, Vcc);
+    _mav_put_uint16_t(buf, 2, Vservo);
+    _mav_put_uint16_t(buf, 4, flags);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_POWER_STATUS_LEN);
+#else
+    mavlink_power_status_t packet;
+    packet.Vcc = Vcc;
+    packet.Vservo = Vservo;
+    packet.flags = flags;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_POWER_STATUS_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_POWER_STATUS;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_POWER_STATUS_MIN_LEN, MAVLINK_MSG_ID_POWER_STATUS_LEN, MAVLINK_MSG_ID_POWER_STATUS_CRC);
+}
+
+/**
+ * @brief Pack a power_status message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param Vcc 5V rail voltage in millivolts
+ * @param Vservo servo rail voltage in millivolts
+ * @param flags power supply status flags (see MAV_POWER_STATUS enum)
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_power_status_pack_chan(uint8_t system_id, u

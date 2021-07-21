321cc28e2906d809ed991f39b0b370fd92e0ae69
Soolj <101229764+Soolj@users.noreply.github.com>
2021-07-21 06:43:17 +0000

    Update azure-pipelines.yml

--- lib/mavlink/common/mavlink_msg_power_status.h
+++ lib/mavlink/common/mavlink_msg_power_status.h
@@ -86,4 +86,44 @@ static inline uint16_t mavlink_msg_power_status_pack(uint8_t system_id, uint8_t 
  * @param flags power supply status flags (see MAV_POWER_STATUS enum)
  * @return length of the message in bytes (excluding serial stream start sign)
  */
-static inline uint16_t mavlink_msg_power_status_pack_chan(uint8_t system_id, u
+static inline uint16_t mavlink_msg_power_status_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   uint16_t Vcc,uint16_t Vservo,uint16_t flags)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_POWER_STATUS_LEN];
+    _mav_put_uint16_t(buf, 0, Vcc);
+    _mav_put_uint16_t(buf, 2, Vservo);
+    _mav_put_uint16_t(buf, 4, flags);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_POWER_STATUS_LEN);
+#else
+    mavlink_power_status_t packet;
+    packet.Vcc = Vcc;
+    packet.Vservo = Vservo;
+    packet.flags = flags;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_POWER_STATUS_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_POWER_STATUS;
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_POWER_STATUS_MIN_LEN, MAVLINK_MSG_ID_POWER_STATUS_LEN, MAVLINK_MSG_ID_POWER_STATUS_CRC);
+}
+
+/**
+ * @brief Encode a power_status struct
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ * @param power_status C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_power_status_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_power_status_t* power_status)
+{
+    return mavlink_msg_power_status_pack(system_id, component_id, msg, power_status->Vcc, power_status->Vservo, power_status->flags);
+}
+
+/**
+ * @brief Encode a power_status struct on a channel
+ *
+ * @param sy

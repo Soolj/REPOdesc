f2d3bb0d1721ce2a39bfd4139bbff00066b9c460
Soolj <101229764+Soolj@users.noreply.github.com>
2021-08-14 20:09:21 +0000

    update data attribute post

--- lib/mavlink/common/mavlink_msg_rc_channels_raw.h
+++ lib/mavlink/common/mavlink_msg_rc_channels_raw.h
@@ -250,4 +250,34 @@ static inline void mavlink_msg_rc_channels_raw_send(mavlink_channel_t chan, uint
     packet.chan1_raw = chan1_raw;
     packet.chan2_raw = chan2_raw;
     packet.chan3_raw = chan3_raw;
-   
+    packet.chan4_raw = chan4_raw;
+    packet.chan5_raw = chan5_raw;
+    packet.chan6_raw = chan6_raw;
+    packet.chan7_raw = chan7_raw;
+    packet.chan8_raw = chan8_raw;
+    packet.port = port;
+    packet.rssi = rssi;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_RC_CHANNELS_RAW, (const char *)&packet, MAVLINK_MSG_ID_RC_CHANNELS_RAW_MIN_LEN, MAVLINK_MSG_ID_RC_CHANNELS_RAW_LEN, MAVLINK_MSG_ID_RC_CHANNELS_RAW_CRC);
+#endif
+}
+
+/**
+ * @brief Send a rc_channels_raw message
+ * @param chan MAVLink channel to send the message
+ * @param struct The MAVLink struct to serialize
+ */
+static inline void mavlink_msg_rc_channels_raw_send_struct(mavlink_channel_t chan, const mavlink_rc_channels_raw_t* rc_channels_raw)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    mavlink_msg_rc_channels_raw_send(chan, rc_channels_raw->time_boot_ms, rc_channels_raw->port, rc_channels_raw->chan1_raw, rc_channels_raw->chan2_raw, rc_channels_raw->chan3_raw, rc_channels_raw->chan4_raw, rc_channels_raw->chan5_raw, rc_channels_raw->chan6_raw, rc_channels_raw->chan7_raw, rc_channels_raw->chan8_raw, rc_channels_raw->rssi);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_RC_CHANNELS_RAW, (const char *)rc_channels_raw, MAVLINK_MSG_ID_RC_CHANNELS_RAW_MIN_LEN, MAVLINK_MSG_ID_RC_CHANNELS_RAW_LEN, MAVLINK_MSG_ID_RC_CHANNELS_RAW_CRC);
+#endif
+}
+
+#if MAVLINK_MSG_ID_RC_CHANNELS_RAW_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is the size of a full 

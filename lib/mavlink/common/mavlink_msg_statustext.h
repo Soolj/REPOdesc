3851911f44eadf4681c2f053584714cd05af04b8
Soolj <101229764+Soolj@users.noreply.github.com>
2021-10-21 14:35:48 +0000

    Bump @types/node in /angular/directives/app-attach-directive

--- lib/mavlink/common/mavlink_msg_statustext.h
+++ lib/mavlink/common/mavlink_msg_statustext.h
@@ -88,4 +88,37 @@ static inline uint16_t mavlink_msg_statustext_pack_chan(uint8_t system_id, uint8
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_STATUSTEXT_LEN);
 #else
     mavlink_statustext_t packet;
-    pac
+    packet.severity = severity;
+    mav_array_memcpy(packet.text, text, sizeof(char)*50);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_STATUSTEXT_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_STATUSTEXT;
+    return mavlink_finalize_message_chan(msg, system_id, component_id, chan, MAVLINK_MSG_ID_STATUSTEXT_MIN_LEN, MAVLINK_MSG_ID_STATUSTEXT_LEN, MAVLINK_MSG_ID_STATUSTEXT_CRC);
+}
+
+/**
+ * @brief Encode a statustext struct
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param msg The MAVLink message to compress the data into
+ * @param statustext C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_statustext_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_statustext_t* statustext)
+{
+    return mavlink_msg_statustext_pack(system_id, component_id, msg, statustext->severity, statustext->text);
+}
+
+/**
+ * @brief Encode a statustext struct on a channel
+ *
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param statustext C-struct to read the message contents from
+ */
+static inline uint16_t mavlink_msg_statustext_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_statustext_t* statustext)
+{
+    return mavlink_msg_statustext_pack_chan(system_id, component_id, chan, msg, statustext->severity, statustex

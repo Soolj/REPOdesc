135b804fed3906ca480176f8ecadcc5c80c33630
Soolj <101229764+Soolj@users.noreply.github.com>
2021-10-21 10:00:22 +0000

    more private internal container

--- lib/mavlink/common/mavlink_msg_statustext.h
+++ lib/mavlink/common/mavlink_msg_statustext.h
@@ -55,4 +55,37 @@ static inline uint16_t mavlink_msg_statustext_pack(uint8_t system_id, uint8_t co
     char buf[MAVLINK_MSG_ID_STATUSTEXT_LEN];
     _mav_put_uint8_t(buf, 0, severity);
     _mav_put_char_array(buf, 1, text, 50);
-        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_STATUSTEXT_LEN);
+#else
+    mavlink_statustext_t packet;
+    packet.severity = severity;
+    mav_array_memcpy(packet.text, text, sizeof(char)*50);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_STATUSTEXT_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_STATUSTEXT;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_STATUSTEXT_MIN_LEN, MAVLINK_MSG_ID_STATUSTEXT_LEN, MAVLINK_MSG_ID_STATUSTEXT_CRC);
+}
+
+/**
+ * @brief Pack a statustext message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param severity Severity of status. Relies on the definitions within RFC-5424. See enum MAV_SEVERITY.
+ * @param text Status text message, without null termination character
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_statustext_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   uint8_t severity,const char *text)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_STATUSTEXT_LEN];
+    _mav_put_uint8_t(buf, 0, severity);
+    _mav_put_char_array(buf, 1, text, 50);
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_STATUSTEXT_LEN);
+#else
+    mavlink_statustext_t packet;
+    pac

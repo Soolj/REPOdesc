ef39cd164b278939e36016674a90e5ccd8d451f2
Soolj <101229764+Soolj@users.noreply.github.com>
2021-11-07 20:21:24 +0000

    removed unwanted files

--- lib/mavlink/common/mavlink_msg_timesync.h
+++ lib/mavlink/common/mavlink_msg_timesync.h
@@ -51,3 +51,45 @@ typedef struct __mavlink_timesync_t {
 static inline uint16_t mavlink_msg_timesync_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
                                int64_t tc1, int64_t ts1)
 {
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_TIMESYNC_LEN];
+    _mav_put_int64_t(buf, 0, tc1);
+    _mav_put_int64_t(buf, 8, ts1);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_TIMESYNC_LEN);
+#else
+    mavlink_timesync_t packet;
+    packet.tc1 = tc1;
+    packet.ts1 = ts1;
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_TIMESYNC_LEN);
+#endif
+
+    msg->msgid = MAVLINK_MSG_ID_TIMESYNC;
+    return mavlink_finalize_message(msg, system_id, component_id, MAVLINK_MSG_ID_TIMESYNC_MIN_LEN, MAVLINK_MSG_ID_TIMESYNC_LEN, MAVLINK_MSG_ID_TIMESYNC_CRC);
+}
+
+/**
+ * @brief Pack a timesync message on a channel
+ * @param system_id ID of this system
+ * @param component_id ID of this component (e.g. 200 for IMU)
+ * @param chan The MAVLink channel this message will be sent over
+ * @param msg The MAVLink message to compress the data into
+ * @param tc1 Time sync timestamp 1
+ * @param ts1 Time sync timestamp 2
+ * @return length of the message in bytes (excluding serial stream start sign)
+ */
+static inline uint16_t mavlink_msg_timesync_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
+                               mavlink_message_t* msg,
+                                   int64_t tc1,int64_t ts1)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_TIMESYNC_LEN];
+    _mav_put_int64_t(buf, 0, tc1);
+    _mav_put_int64_t(buf, 8, ts1);
+
+        memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_TIMESYNC_LEN);
+#else
+    mavlink_timesync_t packet;
+    packet.tc1 = tc1;
+ 

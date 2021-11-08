57cc8d25dc3dac103927c0fd3306be49e4c9cd50
Soolj <101229764+Soolj@users.noreply.github.com>
2021-11-08 10:13:58 +0000

    delete comment

--- lib/mavlink/common/mavlink_msg_timesync.h
+++ lib/mavlink/common/mavlink_msg_timesync.h
@@ -133,4 +133,43 @@ static inline uint16_t mavlink_msg_timesync_encode_chan(uint8_t system_id, uint8
  * @param chan MAVLink channel to send the message
  *
  * @param tc1 Time sync timestamp 1
- * @param t
+ * @param ts1 Time sync timestamp 2
+ */
+#ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+static inline void mavlink_msg_timesync_send(mavlink_channel_t chan, int64_t tc1, int64_t ts1)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    char buf[MAVLINK_MSG_ID_TIMESYNC_LEN];
+    _mav_put_int64_t(buf, 0, tc1);
+    _mav_put_int64_t(buf, 8, ts1);
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TIMESYNC, buf, MAVLINK_MSG_ID_TIMESYNC_MIN_LEN, MAVLINK_MSG_ID_TIMESYNC_LEN, MAVLINK_MSG_ID_TIMESYNC_CRC);
+#else
+    mavlink_timesync_t packet;
+    packet.tc1 = tc1;
+    packet.ts1 = ts1;
+
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TIMESYNC, (const char *)&packet, MAVLINK_MSG_ID_TIMESYNC_MIN_LEN, MAVLINK_MSG_ID_TIMESYNC_LEN, MAVLINK_MSG_ID_TIMESYNC_CRC);
+#endif
+}
+
+/**
+ * @brief Send a timesync message
+ * @param chan MAVLink channel to send the message
+ * @param struct The MAVLink struct to serialize
+ */
+static inline void mavlink_msg_timesync_send_struct(mavlink_channel_t chan, const mavlink_timesync_t* timesync)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+    mavlink_msg_timesync_send(chan, timesync->tc1, timesync->ts1);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_TIMESYNC, (const char *)timesync, MAVLINK_MSG_ID_TIMESYNC_MIN_LEN, MAVLINK_MSG_ID_TIMESYNC_LEN, MAVLINK_MSG_ID_TIMESYNC_CRC);
+#endif
+}
+
+#if MAVLINK_MSG_ID_TIMESYNC_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is

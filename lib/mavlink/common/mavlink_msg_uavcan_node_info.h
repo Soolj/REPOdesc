2922974fa850774b38f9ef8827c3a158dff05369
Soolj <101229764+Soolj@users.noreply.github.com>
2021-11-23 11:57:06 +0000

    email test

--- lib/mavlink/common/mavlink_msg_uavcan_node_info.h
+++ lib/mavlink/common/mavlink_msg_uavcan_node_info.h
@@ -209,4 +209,35 @@ static inline void mavlink_msg_uavcan_node_info_send(mavlink_channel_t chan, uin
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_UAVCAN_NODE_INFO_LEN];
     _mav_put_uint64_t(buf, 0, time_usec);
-    _mav_put_uint32_t(buf, 8,
+    _mav_put_uint32_t(buf, 8, uptime_sec);
+    _mav_put_uint32_t(buf, 12, sw_vcs_commit);
+    _mav_put_uint8_t(buf, 96, hw_version_major);
+    _mav_put_uint8_t(buf, 97, hw_version_minor);
+    _mav_put_uint8_t(buf, 114, sw_version_major);
+    _mav_put_uint8_t(buf, 115, sw_version_minor);
+    _mav_put_char_array(buf, 16, name, 80);
+    _mav_put_uint8_t_array(buf, 98, hw_unique_id, 16);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_UAVCAN_NODE_INFO, buf, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_MIN_LEN, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_LEN, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_CRC);
+#else
+    mavlink_uavcan_node_info_t packet;
+    packet.time_usec = time_usec;
+    packet.uptime_sec = uptime_sec;
+    packet.sw_vcs_commit = sw_vcs_commit;
+    packet.hw_version_major = hw_version_major;
+    packet.hw_version_minor = hw_version_minor;
+    packet.sw_version_major = sw_version_major;
+    packet.sw_version_minor = sw_version_minor;
+    mav_array_memcpy(packet.name, name, sizeof(char)*80);
+    mav_array_memcpy(packet.hw_unique_id, hw_unique_id, sizeof(uint8_t)*16);
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_UAVCAN_NODE_INFO, (const char *)&packet, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_MIN_LEN, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_LEN, MAVLINK_MSG_ID_UAVCAN_NODE_INFO_CRC);
+#endif
+}
+
+/**
+ * @brief Send a uavcan_node_info message
+ * @param chan MAVLink channel to send the message
+ * @param struct The MAVLink struct to serialize
+ */
+static inline void mavlink_msg_uavcan_node_info_send_struct(mavlink_channel_t chan, const mavlink_uavcan_node_info_t* uavcan_node_info)
+{
+#if MAVLINK_NEED_BYTE

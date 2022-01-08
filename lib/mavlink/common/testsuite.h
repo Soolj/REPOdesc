450a2d16a53c1c4973f8f8438b6d2660061cfa55
Soolj <101229764+Soolj@users.noreply.github.com>
2022-01-08 17:37:15 +0000

    added command

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -81,4 +81,39 @@ static void mavlink_test_heartbeat(uint8_t system_id, uint8_t component_id, mavl
         memset(&packet2, 0, sizeof(packet2));
     mavlink_msg_heartbeat_send(MAVLINK_COMM_1 , packet1.type , packet1.autopilot , packet1.base_mode , packet1.custom_mode , packet1.system_status );
     mavlink_msg_heartbeat_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&pac
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_sys_status(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_SYS_STATUS >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_sys_status_t packet_in = {
+        963497464,963497672,963497880,17859,17963,18067,18171,18275,18379,18483,18587,18691,223
+    };
+    mavlink_sys_status_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.onboard_control_sensors_present = packet_in.onboard_control_sensors_present;
+        packet1.onboard_control_sensors_enabled = packet_in.onboard_control_sensors_enabled;
+        packet1.onboard_control_sensors_health = packet_in.onboard_control_sensors_health;
+        packet1.load = packet_in.load;
+        packet1.voltage_battery = packet_in.voltage_battery;
+        packet1.current_battery = packet_in.current_battery;
+        packet1.drop_rate_comm = packet_in.drop_rate_comm;
+        packet1.errors_comm = packet_in.errors_comm;
+        packet1.errors_count1 = packet_in.errors_count1;
+        packet1.errors_count2 = packet_in.errors_count2;
+        packet1.errors_count3 = packet_in.errors_count3;
+        packet1.errors_count4 = packet_in.errors_count4;
+        packet1.battery_remaining = packet_in.battery_remaining;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (

bd4cbb7f9c7d5006b437cb0776c5062cafecc538
Soolj <101229764+Soolj@users.noreply.github.com>
2022-01-14 20:47:05 +0000

    hadoop

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -421,4 +421,44 @@ static void mavlink_test_auth_key(uint8_t system_id, uint8_t component_id, mavli
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
     mavlink_msg_auth_key_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, s
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_auth_key_send(MAVLINK_COMM_1 , packet1.key );
+    mavlink_msg_auth_key_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_set_mode(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_SET_MODE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_set_mode_t packet_in = {
+        963497464,17,84
+    };
+    mavlink_set_mode_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.custom_mode = packet_in.custom_mode;
+        packet1.target_system = packet_in.target_system;
+        packet1.base_mode = packet_in.base_mode;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_SET_MODE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_SET_MODE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_set_mode_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_set_mode_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, 

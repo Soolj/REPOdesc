8cb0971a5a6a1709929b788eb8cad4a352565844
Soolj <101229764+Soolj@users.noreply.github.com>
2022-02-28 18:43:50 +0000

    test output

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -1454,4 +1454,38 @@ static void mavlink_test_rc_channels_raw(uint8_t system_id, uint8_t component_id
 
 static void mavlink_test_servo_output_raw(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-#ifdef MAVLINK_STATU
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_SERVO_OUTPUT_RAW >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_servo_output_raw_t packet_in = {
+        963497464,17443,17547,17651,17755,17859,17963,18067,18171,65,18327,18431,18535,18639,18743,18847,18951,19055
+    };
+    mavlink_servo_output_raw_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_usec = packet_in.time_usec;
+        packet1.servo1_raw = packet_in.servo1_raw;
+        packet1.servo2_raw = packet_in.servo2_raw;
+        packet1.servo3_raw = packet_in.servo3_raw;
+        packet1.servo4_raw = packet_in.servo4_raw;
+        packet1.servo5_raw = packet_in.servo5_raw;
+        packet1.servo6_raw = packet_in.servo6_raw;
+        packet1.servo7_raw = packet_in.servo7_raw;
+        packet1.servo8_raw = packet_in.servo8_raw;
+        packet1.port = packet_in.port;
+        packet1.servo9_raw = packet_in.servo9_raw;
+        packet1.servo10_raw = packet_in.servo10_raw;
+        packet1.servo11_raw = packet_in.servo11_raw;
+        packet1.servo12_raw = packet_in.servo12_raw;
+        packet1.servo13_raw = packet_in.servo13_raw;
+        packet1.servo14_raw = packet_in.servo14_raw;
+        packet1.servo15_raw = packet_in.servo15_raw;
+        packet1.servo16_raw = packet_in.servo16_raw;
+        
+        
+#ifdef MAVLINK_STATUS

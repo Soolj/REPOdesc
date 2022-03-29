1d1242041c55532b2697dab100b55ce527770311
Soolj <101229764+Soolj@users.noreply.github.com>
2022-03-29 16:32:10 +0000

    update README.MD

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -2573,4 +2573,38 @@ static void mavlink_test_nav_controller_output(uint8_t system_id, uint8_t compon
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
     mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STA
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_nav_controller_output_t packet_in = {
+        17.0,45.0,73.0,101.0,129.0,18275,18379,18483
+    };
+    mavlink_nav_controller_output_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.nav_roll = packet_in.nav_roll;
+        packet1.nav_pitch = packet_in.nav_pitch;
+        packet1.alt_error = packet_in.alt_error;
+        packet1.aspd_error = packet_in.aspd_error;
+        packet1.xtrack_error = packet_in.xtrack_error;
+        packet1.nav_bearing = packet_in.nav_bearing;
+        packet1.target_bearing = packet_in.target_bearing;
+        packet1.wp_dist = packet_in.wp_dist;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_nav_controller_output_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_nav_controller_output_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_nav_controller_output_pack(system_id, component_id

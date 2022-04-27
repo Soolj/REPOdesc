3d4e47cd8918475db4c619ada3eafa4c95e4f496
Soolj <101229764+Soolj@users.noreply.github.com>
2022-04-27 08:13:25 +0000

    chore(version): Bump version number

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -3585,4 +3585,31 @@ static void mavlink_test_set_position_target_local_ned(uint8_t system_id, uint8_
         packet1.time_boot_ms = packet_in.time_boot_ms;
         packet1.x = packet_in.x;
         packet1.y = packet_in.y;
-        packet1.z = 
+        packet1.z = packet_in.z;
+        packet1.vx = packet_in.vx;
+        packet1.vy = packet_in.vy;
+        packet1.vz = packet_in.vz;
+        packet1.afx = packet_in.afx;
+        packet1.afy = packet_in.afy;
+        packet1.afz = packet_in.afz;
+        packet1.yaw = packet_in.yaw;
+        packet1.yaw_rate = packet_in.yaw_rate;
+        packet1.type_mask = packet_in.type_mask;
+        packet1.target_system = packet_in.target_system;
+        packet1.target_component = packet_in.target_component;
+        packet1.coordinate_frame = packet_in.coordinate_frame;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_SET_POSITION_TARGET_LOCAL_NED_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_SET_POSITION_TARGET_LOCAL_NED_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_set_position_target_local_ned_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_set_position_target_local_ned_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_set_position_target_local_ned_pack(system_id, component_id, &msg , packet1.time_boot_ms , packet1.target_system , packet1.target_component , packet1.coordinate_frame , packet1.type_mask , packet1.x , packet1.y , packet1.z , packet1.vx , packet1.vy , packet1.vz , packet1.afx , packet1.afy , packet1.afz 

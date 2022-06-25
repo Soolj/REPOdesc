7e7233aea70d3239f73d2d251f8e4f6c634b480c
Soolj <101229764+Soolj@users.noreply.github.com>
2022-06-25 23:06:59 +0000

    Add a Dockerfile

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -4821,4 +4821,30 @@ static void mavlink_test_file_transfer_protocol(uint8_t system_id, uint8_t compo
         memset(&packet2, 0, sizeof(packet2));
     mavlink_msg_file_transfer_protocol_encode(system_id, component_id, &msg, &packet1);
     mavlink_msg_file_transfer_protocol_decode(&msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&pack
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_file_transfer_protocol_pack(system_id, component_id, &msg , packet1.target_network , packet1.target_system , packet1.target_component , packet1.payload );
+    mavlink_msg_file_transfer_protocol_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_file_transfer_protocol_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.target_network , packet1.target_system , packet1.target_component , packet1.payload );
+    mavlink_msg_file_transfer_protocol_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_file_transfer_protocol_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_file_transfer_protocol_send(MAVLINK_COMM_1 , packet1.target_network , packet1.target_system , packet1.target_component , packet1.payload );
+    mavlink_msg_file_transfer_protocol_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_timesync(uint8_t system_id, uint8_t compon

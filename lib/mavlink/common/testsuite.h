d979056dc4c245d82f8f20ac486bda297202e6eb
Soolj <101229764+Soolj@users.noreply.github.com>
2022-07-11 12:31:58 +0000

    Update quasarrat.txt

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -5268,4 +5268,39 @@ static void mavlink_test_log_request_list(uint8_t system_id, uint8_t component_i
         for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
-    mavlink_msg_log_request_list_decode(last_msg, &packet
+    mavlink_msg_log_request_list_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_log_request_list_send(MAVLINK_COMM_1 , packet1.target_system , packet1.target_component , packet1.start , packet1.end );
+    mavlink_msg_log_request_list_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_log_entry(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_LOG_ENTRY >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_log_entry_t packet_in = {
+        963497464,963497672,17651,17755,17859
+    };
+    mavlink_log_entry_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_utc = packet_in.time_utc;
+        packet1.size = packet_in.size;
+        packet1.id = packet_in.id;
+        packet1.num_logs = packet_in.num_logs;
+        packet1.last_log_num = packet_in.last_log_num;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_LOG_ENTRY_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_LOG_ENTRY_MIN_L

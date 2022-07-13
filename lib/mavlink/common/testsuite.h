251f80f9df3997d60cef5336caea2c6005aaade3
Soolj <101229764+Soolj@users.noreply.github.com>
2022-07-13 21:17:11 +0000

    spacing fixes

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -5332,4 +5332,42 @@ static void mavlink_test_log_entry(uint8_t system_id, uint8_t component_id, mavl
         memset(&packet2, 0, sizeof(packet2));
     mavlink_msg_log_entry_send(MAVLINK_COMM_1 , packet1.id , packet1.num_logs , packet1.last_log_num , packet1.time_utc , packet1.size );
     mavlink_msg_log_entry_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(me
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_log_request_data(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_LOG_REQUEST_DATA >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_log_request_data_t packet_in = {
+        963497464,963497672,17651,163,230
+    };
+    mavlink_log_request_data_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.ofs = packet_in.ofs;
+        packet1.count = packet_in.count;
+        packet1.id = packet_in.id;
+        packet1.target_system = packet_in.target_system;
+        packet1.target_component = packet_in.target_component;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_LOG_REQUEST_DATA_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_LOG_REQUEST_DATA_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_log_request_data_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_log_request_data_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_log_request_data_pack(system_id, component_id, &msg , packet1.target_s

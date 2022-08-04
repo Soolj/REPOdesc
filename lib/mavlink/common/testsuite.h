df58525c53379bd4c301e65ea3e653826c916468
Soolj <101229764+Soolj@users.noreply.github.com>
2022-08-04 05:32:46 +0000

    Update test.md

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -5996,4 +5996,38 @@ static void mavlink_test_data_transmission_handshake(uint8_t system_id, uint8_t 
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
     mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STA
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_DATA_TRANSMISSION_HANDSHAKE >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_data_transmission_handshake_t packet_in = {
+        963497464,17443,17547,17651,163,230,41
+    };
+    mavlink_data_transmission_handshake_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.size = packet_in.size;
+        packet1.width = packet_in.width;
+        packet1.height = packet_in.height;
+        packet1.packets = packet_in.packets;
+        packet1.type = packet_in.type;
+        packet1.payload = packet_in.payload;
+        packet1.jpg_quality = packet_in.jpg_quality;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_DATA_TRANSMISSION_HANDSHAKE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_DATA_TRANSMISSION_HANDSHAKE_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_data_transmission_handshake_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_data_transmission_handshake_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_data_transmission_handshake_pack(system_id, component_id, &msg , packet1.type , packet1.size , packet1.width , packet1.height , packet1.packets , packet1.payload , packet1.jpg_quality );
+    mavlink_msg_data_transmissi

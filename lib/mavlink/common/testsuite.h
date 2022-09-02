6ffce6ecbddd1a83bf972d819f453c1c030dc09f
Soolj <101229764+Soolj@users.noreply.github.com>
2022-09-02 12:36:33 +0000

    welcome message

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -7379,4 +7379,40 @@ static void mavlink_test_gps_rtcm_data(uint8_t system_id, uint8_t component_id, 
         memset(&packet2, 0, sizeof(packet2));
     mavlink_msg_gps_rtcm_data_send(MAVLINK_COMM_1 , packet1.flags , packet1.len , packet1.data );
     mavlink_msg_gps_rtcm_data_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0)
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_high_latency(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_HIGH_LATENCY >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_high_latency_t packet_in = {
+        963497464,963497672,963497880,17859,17963,18067,18171,18275,18379,18483,211,22,89,156,223,34,101,168,235,46,113,180,247,58
+    };
+    mavlink_high_latency_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.custom_mode = packet_in.custom_mode;
+        packet1.latitude = packet_in.latitude;
+        packet1.longitude = packet_in.longitude;
+        packet1.roll = packet_in.roll;
+        packet1.pitch = packet_in.pitch;
+        packet1.heading = packet_in.heading;
+        packet1.heading_sp = packet_in.heading_sp;
+        packet1.altitude_amsl = packet_in.altitude_amsl;
+        packet1.altitude_sp = packet_in.altitude_sp;
+        packet1.wp_distance = packet_in.wp_distance;
+        packet1.base_mode = packet_in.base_mode;
+        packet1.landed_state = packet_in.landed_state;
+        packet1.throttle = packet_in.throttle;
+        packet1.airspeed = packet_in.airspeed;
+        packet1.airspeed_sp = packet_in.airspeed_sp;
+        packet1.groundspeed = packet_in.groundspeed;
+        packet1.climb_rate = packet_in.climb_rate;
+ 

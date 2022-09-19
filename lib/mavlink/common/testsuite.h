f0173882b8010807f55898c6e17af93c23f0498a
Soolj <101229764+Soolj@users.noreply.github.com>
2022-09-19 16:42:28 +0000

    remoteproc: select VIRTIO to avoid build breakage

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -8071,4 +8071,42 @@ static void mavlink_test_memory_vect(uint8_t system_id, uint8_t component_id, ma
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
     mavlink_msg_memory_vect_decode(last_msg, &packet2);
-        MAVLINK_ASSERT(memcm
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_memory_vect_send(MAVLINK_COMM_1 , packet1.address , packet1.ver , packet1.type , packet1.value );
+    mavlink_msg_memory_vect_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
+static void mavlink_test_debug_vect(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_DEBUG_VECT >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_debug_vect_t packet_in = {
+        93372036854775807ULL,73.0,101.0,129.0,"UVWXYZABC"
+    };
+    mavlink_debug_vect_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_usec = packet_in.time_usec;
+        packet1.x = packet_in.x;
+        packet1.y = packet_in.y;
+        packet1.z = packet_in.z;
+        
+        mav_array_memcpy(packet1.name, packet_in.name, sizeof(char)*10);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_DEBUG_VECT_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_DEBUG_VECT_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_debug_vect

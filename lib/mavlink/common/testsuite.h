18452f9aa9dbf6038760e90a9ac9b05df8684d85
Soolj <101229764+Soolj@users.noreply.github.com>
2022-10-06 09:00:17 +0000

    Compiler warnings

--- lib/mavlink/common/testsuite.h
+++ lib/mavlink/common/testsuite.h
@@ -8835,4 +8835,38 @@ static void mavlink_test_camera_image_captured(uint8_t system_id, uint8_t compon
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
-static void mavlink_test_flight_informat
+static void mavlink_test_flight_information(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_FLIGHT_INFORMATION >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_flight_information_t packet_in = {
+        93372036854775807ULL,93372036854776311ULL,93372036854776815ULL,963498712
+    };
+    mavlink_flight_information_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.arming_time_utc = packet_in.arming_time_utc;
+        packet1.takeoff_time_utc = packet_in.takeoff_time_utc;
+        packet1.flight_uuid = packet_in.flight_uuid;
+        packet1.time_boot_ms = packet_in.time_boot_ms;
+        
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_FLIGHT_INFORMATION_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_FLIGHT_INFORMATION_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_flight_information_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_flight_information_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_flight_information_

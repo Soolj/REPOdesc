3dcb718f3cc2e6230c035ffb929db6afdc0cc3a2
Soolj <101229764+Soolj@users.noreply.github.com>
2022-11-01 17:07:38 +0000

    Bug fixes

--- lib/mavlink/mavlink_helpers.h
+++ lib/mavlink/mavlink_helpers.h
@@ -71,4 +71,56 @@ MAVLINK_HELPER uint8_t mavlink_sign_packet(mavlink_signing_t *signing,
 	union {
 	    uint64_t t64;
 	    uint8_t t8[8];
-	} tstam
+	} tstamp;
+	if (signing == NULL || !(signing->flags & MAVLINK_SIGNING_FLAG_SIGN_OUTGOING)) {
+	    return 0;
+	}
+	signature[0] = signing->link_id;
+	tstamp.t64 = signing->timestamp;
+	memcpy(&signature[1], tstamp.t8, 6);
+	signing->timestamp++;
+
+	mavlink_sha256_init(&ctx);
+	mavlink_sha256_update(&ctx, signing->secret_key, sizeof(signing->secret_key));
+	mavlink_sha256_update(&ctx, header, header_len);
+	mavlink_sha256_update(&ctx, packet, packet_len);
+	mavlink_sha256_update(&ctx, crc, 2);
+	mavlink_sha256_update(&ctx, signature, 7);
+	mavlink_sha256_final_48(&ctx, &signature[7]);
+
+	return MAVLINK_SIGNATURE_BLOCK_LEN;
+}
+
+/**
+ * return new packet length for trimming payload of any trailing zero
+ * bytes. Used in MAVLink2 to give simple support for variable length
+ * arrays.
+ */
+MAVLINK_HELPER uint8_t _mav_trim_payload(const char *payload, uint8_t length)
+{
+	while (length > 1 && payload[length-1] == 0) {
+		length--;
+	}
+	return length;
+}
+
+/**
+ * @brief check a signature block for a packet
+ */
+MAVLINK_HELPER bool mavlink_signature_check(mavlink_signing_t *signing,
+					    mavlink_signing_streams_t *signing_streams,
+					    const mavlink_message_t *msg)
+{
+	if (signing == NULL) {
+		return true;
+	}
+        const uint8_t *p = (const uint8_t *)&msg->magic;
+	const uint8_t *psig = msg->signature;
+        const uint8_t *incoming_signature = psig+7;
+	mavlink_sha256_ctx ctx;
+	uint8_t signature[6];
+	uint16_t i;
+
+	mavlink_sha256_init(&ctx);
+	mavlink_sha256_update(&ctx, signing->secret_key, sizeof(signing->secret_key));
+	mavlink_sha256_update(&ctx, p, MAVLINK_CORE_HEADER_

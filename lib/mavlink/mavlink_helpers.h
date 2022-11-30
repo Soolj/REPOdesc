e6eef915f6bee24d1546038fc3e559153214da5b
Soolj <101229764+Soolj@users.noreply.github.com>
2022-11-30 19:05:51 +0000

    remove unnecessary logs

--- lib/mavlink/mavlink_helpers.h
+++ lib/mavlink/mavlink_helpers.h
@@ -1118,4 +1118,13 @@ MAVLINK_HELPER void _mavlink_send_uart(mavlink_channel_t chan, const char *buf, 
 #else
 	/* fallback to one byte at a time */
 	uint16_t i;
-	for (i = 0; i < len; i+
+	for (i = 0; i < len; i++) {
+		comm_send_ch(chan, (uint8_t)buf[i]);
+	}
+#endif
+}
+#endif // MAVLINK_USE_CONVENIENCE_FUNCTIONS
+
+#ifdef MAVLINK_USE_CXX_NAMESPACE
+} // namespace mavlink
+#endif

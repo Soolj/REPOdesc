802d3f968f3bdb3194b5c6e8bd30fb8589173b5c
Soolj <101229764+Soolj@users.noreply.github.com>
2022-12-13 14:13:25 +0000

    Update install

--- src/Drawing/WindowThreshold.h
+++ src/Drawing/WindowThreshold.h
@@ -24,4 +24,70 @@ public:
     {
       if (_active)
       {
- 
+        printf("PASS: ABS(%s) was less than %lf for at least %lf seconds\n", _var.c_str(), _thresh, _minWindow);
+      }
+      else
+      {
+        printf("FAIL: ABS(%s) was less than %lf for %lf seconds, which was less than %lf seconds\n", _var.c_str(), _thresh, _lastTime- _lastTimeAboveThresh, _minWindow);
+      }
+    }
+    _lastTimeAboveThresh = numeric_limits<float>::infinity();
+    _active = false;
+  }
+
+  void Update(double time, std::vector<shared_ptr<DataSource> >& sources)
+  {
+    for (unsigned int j = 0; j < sources.size(); j++)
+    {
+      float tmp;
+      if (sources[j]->GetData(_var, tmp))
+      {
+        OnNewData((float)time, tmp);
+        break;
+      }
+    }
+  }
+
+  void OnNewData(float time, float meas)
+  {
+    _lastTime = time;
+    if (_active)
+    {
+      return;
+    }
+
+    if (_lastTimeAboveThresh == numeric_limits<float>::infinity())
+    {
+      _lastTimeAboveThresh = time;
+    }
+
+    if (fabs(meas) > _thresh)
+    {
+      _lastTimeAboveThresh = time;
+    }
+
+    if ((time - _lastTimeAboveThresh) > _minWindow)
+    {
+      _active = true;
+    }
+  }
+
+  // Draws horizontal threshold bands
+  // and detection marker/time
+  void Draw(float minX, float maxX, float minY, float maxY)
+  {
+    glColor3f(.1f, .2f, .1f);
+
+    if (_thresh > minY && _thresh < maxY)
+    {
+      glBegin(GL_LINES);
+      glVertex2f(minX, _thresh);
+      glVertex2f(maxX, _thresh);
+      glEnd();
+    }
+
+    if ((-_thresh) > minY && (-_thresh) < maxY)
+    {
+      glBegin(GL_LINES);
+      glVertex2f(minX, -_thresh);
+      glVertex2f(maxX, -_thresh)

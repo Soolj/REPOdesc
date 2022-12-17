34bdc64c33439d0eb992a5fe5dec1d2c47cce378
Soolj <101229764+Soolj@users.noreply.github.com>
2022-12-17 22:35:36 +0000

    IGN:Updated translations

--- src/Math/V3D.h
+++ src/Math/V3D.h
@@ -56,4 +56,58 @@ public:
   inline V3D operator/=(const V3D b){ *this = *this / b; return *this;}
   inline V3D operator*=(const V3D b){ *this = *this * b; return *this;}
   inline V3D operator+=(const V3D b){ *this = *this + b; return *this;}
-  inline V3D operator-=(const V
+  inline V3D operator-=(const V3D b){ *this = *this - b; return *this;}
+
+  inline V3D sq() const{ return operator*(*this);}
+  inline V3D sqrt() const{ return V3D(::sqrt(x),::sqrt(y),::sqrt(z));}
+	
+	inline V3D operator - () const { return V3D(-x, -y, -z); }
+
+	inline void constrain(double lowXYZ,double highXYZ)
+	{
+		constrain(lowXYZ,highXYZ,lowXYZ,highXYZ,lowXYZ,highXYZ);
+	}
+	inline void constrain(double lowX,double highX, double lowY, double highY, double lowZ, double highZ)
+	{
+		x = CONSTRAIN(x,lowX,highX);
+		y = CONSTRAIN(y,lowY,highY);
+		z = CONSTRAIN(z,lowZ,highZ);
+	}
+
+  inline double mag() const{return ::sqrt(x*x+y*y+z*z);  }
+	inline double magSq() const{return (x*x+y*y+z*z);  }
+  inline double magXY() const{return ::sqrt(x*x+y*y); }
+  inline double norminf() const{return MAX(MAX(fabs(x),fabs(y)),fabs(z));}
+
+  inline bool isZero() const{return (x==0.0 && y==0.0 && z==0.0);};
+
+  inline void zero(){x=y=z=0.0;};
+
+  static V3D Inf() {
+    return V3D(numeric_limits<double>::infinity(),numeric_limits<double>::infinity(),numeric_limits<double>::infinity());
+  }
+
+	// projects this vector onto b and return the magnitude of the resulting vector
+	inline double projectMag(const V3D& b)
+	{
+		return (this->dot(b))/b.magSq();
+	}
+
+	// return projection of this vector onto b 
+	inline V3D projectOnto(const V3D& b)
+	{
+		return b.norm() * (this->dot(b.norm()));
+	}
+
+  inline double operator[](const int i) const {
+    switch (i) {
+      case 0: return x;
+      case 1: return y;
+      default: return z;
+    }
+  }
+	
+  inline double& operator[](const int i){
+    switch(i){
+      case 0: return x;
+  

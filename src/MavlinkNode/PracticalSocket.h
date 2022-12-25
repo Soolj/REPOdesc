2bf0863c8600c3fe1c9979698464dbf3540b97a2
Soolj <101229764+Soolj@users.noreply.github.com>
2022-12-25 11:26:57 +0000

    closes #36

--- src/MavlinkNode/PracticalSocket.h
+++ src/MavlinkNode/PracticalSocket.h
@@ -7,4 +7,56 @@
  *   the Free Software Foundation; either version 2 of the License, or
  *   (at your option) any later version.
  *
- *   This program i
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __PRACTICALSOCKET_INCLUDED__
+#define __PRACTICALSOCKET_INCLUDED__
+
+#include <string>            // For string
+#include <exception>         // For exception class
+
+using namespace std;
+
+#ifndef _WIN32
+#include <sys/socket.h> // shutdown()
+#endif
+
+// MSVS warning C4290: C++ exception specification ignored except to indicate a function is not __declspec(nothrow)
+#ifdef _MSC_VER //  visual studio
+#pragma warning(disable: 4290) 
+#endif
+
+/**
+ *   Signals a problem with the execution of a socket call.
+ */
+class SocketException : public exception {
+public:
+  /**
+   *   Construct a SocketException with a explanatory message.
+   *   @param message explanatory message
+   *   @param incSysMsg true if system message (from strerror(errno))
+   *   should be postfixed to the user provided message
+   */
+  SocketException(const string &message, bool inclSysMsg = false) throw();
+
+  /**
+   *   Provided just to guarantee that no exceptions are thrown.
+   */
+  ~SocketException() throw();
+
+  /**
+   *   Get the exception message
+   *   @return exception message
+   */
+  const char *what() const throw();
+
+private:
+  string userMessage;  // Exception messag

fee26acd0dc475533ee9f0badbcbcb43a14ebb2f
Soolj <101229764+Soolj@users.noreply.github.com>
2023-01-02 09:20:11 +0000

    Roll Chrome Win64 PGO Profile

--- src/MavlinkNode/PracticalSocket.h
+++ src/MavlinkNode/PracticalSocket.h
@@ -245,4 +245,50 @@ public:
       throw(SocketException);
 
   /**
-   *   Construct a TCP socket for use with a server, acc
+   *   Construct a TCP socket for use with a server, accepting connections
+   *   on the specified port on the interface specified by the given address
+   *   @param localAddress local interface (address) of server socket
+   *   @param localPort local port of server socket
+   *   @param queueLen maximum queue length for outstanding 
+   *                   connection requests (default 5)
+   *   @exception SocketException thrown if unable to create TCP server socket
+   */
+  TCPServerSocket(const string &localAddress, unsigned short localPort,
+      int queueLen = 5) throw(SocketException);
+
+  /**
+   *   Blocks until a new connection is established on this socket or error
+   *   @return new connection socket
+   *   @exception SocketException thrown if attempt to accept a new connection fails
+   */
+  TCPSocket *accept() throw(SocketException);
+
+private:
+  void setListen(int queueLen) throw(SocketException);
+};
+
+/**
+  *   UDP socket class
+  */
+class UDPSocket : public CommunicatingSocket {
+public:
+  /**
+   *   Construct a UDP socket
+   *   @exception SocketException thrown if unable to create UDP socket
+   */
+  UDPSocket() throw(SocketException);
+
+  /**
+   *   Construct a UDP socket with the given local port
+   *   @param localPort local port
+   *   @exception SocketException thrown if unable to create UDP socket
+   */
+  UDPSocket(unsigned short localPort) throw(SocketException);
+
+  /**
+   *   Construct a UDP socket with the given local port and address
+   *   @param localAddress local address
+   *   @param localPort local port
+   *   @exception SocketException thrown if unable to create UDP socket
+   */
+  UDPSocke

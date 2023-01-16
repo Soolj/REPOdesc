d86c9e32532c8298b7db39e351dc53c2cf7d1df5
Soolj <101229764+Soolj@users.noreply.github.com>
2023-01-16 22:47:20 +0000

    reset bower.json to #master

--- src/Simulation/QuadDynamics.h
+++ src/Simulation/QuadDynamics.h
@@ -27,4 +27,49 @@ public:
 	virtual ~QuadDynamics() {}; // destructor
 	virtual int Initialize();
 
-  virtual void Run(float dt, float simulationTime
+  virtual void Run(float dt, float simulationTime, int &idum,  // updates the simulation
+      V3F externalForceInGlobalFrame = V3F(),    // required to take net forces into account
+      V3F externalMomentInBodyFrame = V3F());   // required to take net moments into account
+                  
+	virtual void SetCommands(const VehicleCommand& cmd);	// update commands in the simulator coming from a command2 packet
+
+  virtual void Dynamics(float dt, float simTime, V3F external_force, V3F external_moment, int& idum);
+
+	double GetRotDistInt() {return rotDisturbanceInt;};
+	double GetXyzDistInt() {return xyzDisturbanceInt;};
+	double GetRotDistBW() {return rotDisturbanceBW;};
+	double GetXyzDistBW() {return xyzDisturbanceBW;};
+	double GetGyroNoiseInt() {return gyroNoiseInt;};
+
+  virtual bool GetData(const string& name, float& ret) const;
+  virtual vector<string> GetFields() const;
+
+  void Reset();
+
+	VehicleCommand GetCommands() const { return curCmd; }
+
+  void ResetState(V3F pos=V3F(), V3F vel=V3F(), Quaternion<float> att=Quaternion<float>(), V3F omega=V3F());
+  void SetPosVelAttOmega(V3F pos=V3F(), V3F vel=V3F(), Quaternion<float> att=Quaternion<float>(), V3F omega=V3F()); 
+
+	void TurnOffNonidealities();
+
+	void RunRoomConstraints(const V3F& oldPos);
+  
+	VehicleCommand curCmd;
+
+	FastDelegate4<V3F, V3F, Quaternion<float>, V3F> updateIdealStateCallback;
+
+
+  FastDelegate1<TrajectoryPoint> followedTrajectoryCallback;
+
+	float GetArmLength() const { return L; }
+
+  ControllerHandle controller;
+  
+  friend class Visualizer_GLUT;
+
+protected:
+  matrix::Vector<float, 4> motorCmdsN;
+  matrix::Vector<float, 4> motorCmdsOld;
+
+	// useful matrices/vectors that are recomputed from the

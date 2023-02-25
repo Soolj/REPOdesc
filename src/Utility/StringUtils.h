55aa7585e810d23908c1fc51869c38bf8eba8ee2
Soolj <101229764+Soolj@users.noreply.github.com>
2023-02-25 21:26:44 +0000

    Updated Maison A Vendre45 and 36 other files

--- src/Utility/StringUtils.h
+++ src/Utility/StringUtils.h
@@ -39,4 +39,58 @@ inline std::string ToLower(const std::string& in)
 }
 
 
-inline std::stri
+inline std::string CapitalizeFirstLetter(const std::string& in)
+{
+  std::string ret=in;
+  if(in.size()>0)
+  { 
+    if(ret[0]>='a' && ret[0]<='z') ret[0] -= ('a'-'A');
+  }  
+  return ret;
+}
+
+inline bool Contains(const std::string& s, char c)
+{
+  const auto i = s.find_first_of(c);
+  return i != std::string::npos;
+}
+
+inline std::string LeftOf(const std::string& s, char c)
+{
+  const auto i = s.find_first_of(c);
+  if (i == std::string::npos) return s;
+  return s.substr(0, i);
+}
+
+inline std::string RightOf(const std::string& s, char c)
+{
+  const auto i = s.find_first_of(c);
+  if (i == std::string::npos) return "";
+  return s.substr(i + 1);
+}
+
+inline std::vector<string> Split(const char* str, char c = ' ')
+{
+  std::vector<std::string> result;
+
+  do
+  {
+    const char *begin = str;
+
+    while (*str != c && *str)
+    {
+      str++;
+    }
+
+    result.push_back(std::string(begin, str));
+  } while (0 != *str++);
+
+  return result;
+}
+
+inline std::vector<std::string> Split(std::string s, char c = ' ')
+{
+  return Split(s.c_str(), c);
+}
+
+} // namespace SLR
